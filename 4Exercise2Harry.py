87182b6dfcc6a1d5d85ff1d494ac1aa7e5956905
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-18 17:16:25 +0530

    Create 4Exercise2Harry.py

---
+++ 4Exercise2Harry.py
@@ -0,0 +1,20 @@
+x = float(input("entre the first No.: "))
+y = input("entre the operater: ")
+z = float(input("entre the Second No.: "))
+
+if (x == 45 and z == 3 and y == "*") or (z == 45 and x == 3 and y == "*"):
+    print(555)
+elif (x == 56 and z == 9 and y == "+") or (z == 56 and x == 9 and y == "+"):
+    print(77)
+elif (x == 56 and z == 6 and y == "/") or (z == 56 and x == 6 and y == "/"):
+    print(4)
+elif y == "+":
+    print(x + z)
+elif y == "-":
+    print(x - z)
+elif y == "*":
+    print(x * z)
+elif y == "/":
+    print(x / z)
+else:
+    print("invalid Input!")
