413b142c1aee4dc714d864a13484da5347f9685e
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-18 17:26:09 +0530

    Update 3Midlectureexcersideharry.py

--- 3Midlectureexcersideharry.py
+++ 3Midlectureexcersideharry.py
@@ -1,7 +1,9 @@
 x = int(input("your age please:"))
-if x > 18:
+if x > 18 and x < 101:
     print("you can drive")
 elif x == 18:
     print("we cannot tell if you can drive, you have to come and take a test for it")
-else:
+elif x < 18 and x < 4:
     print("sorry you cannot drive")
+else:
+    print("Illogical Input!!!")
