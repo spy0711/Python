69867259c406224dffaa8c029cc2d37afda31253
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-18 15:49:16 +0530

    Create Exercise1Harry.py

---
+++ Exercise1Harry.py
@@ -0,0 +1,4 @@
+dict = {"a": "first letter", "b": "2nd letter", "c": "3rd letter", "d": " 4th letter"}
+#x = input("Ask something from dict:")
+#print(dict[x])
+print(dict[input("Ask something from dict:")])
