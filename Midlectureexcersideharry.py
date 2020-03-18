10648e57298bc2e71a785d2830135c14bb5984c9
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-18 16:16:29 +0530

    Create Midlectureexcersideharry.py

---
+++ Midlectureexcersideharry.py
@@ -0,0 +1,8 @@
+x = input("your age please:")
+y = int(x)
+if int(x)>18:
+    print("you can drive")
+elif y==18:
+    print("we cannot tell if you can drive, you have to come and take a test for it")
+else :
+    print("sorry you cannot drive")
