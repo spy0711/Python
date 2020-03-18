a456709c03e0cbab4b2d5d1b4240813a3614d941
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-18 16:42:37 +0530

    Update Midlectureexcersideharry.py

--- Midlectureexcersideharry.py
+++ Midlectureexcersideharry.py
@@ -1,8 +1,7 @@
-x = input("your age please:")
-y = int(x)
-if int(x)>18:
+x = int(input("your age please:"))
+if x > 18:
     print("you can drive")
-elif y==18:
+elif x == 18:
     print("we cannot tell if you can drive, you have to come and take a test for it")
-else :
+else:
     print("sorry you cannot drive")
