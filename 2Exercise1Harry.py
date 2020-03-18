072d332eb26ea54ac576c841826feae9c0c7d046
Shbhm-0711 <56531911+Shbhm-0711@users.noreply.github.com>
2020-03-18 17:20:50 +0530

    Update 2Exercise1Harry.py

--- 2Exercise1Harry.py
+++ 2Exercise1Harry.py
@@ -1,4 +1,4 @@
-dict = {"a": "first letter", "b": "2nd letter", "c": "3rd letter", "d": " 4th letter"}
+dict = {"a": "1st letter", "b": "2nd letter", "c": "3rd letter", "d": " 4th letter"}
 #x = input("Ask something from dict:")
 #print(dict[x])
 print(dict[input("Ask something from dict:")])
