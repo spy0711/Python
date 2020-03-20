331b280225ecd158e312f74e1e72430302ab4689
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-20 23:19:48 +0530

    Update 5Midlecturequizharry_loops.py

--- 5Midlecturequizharry_loops.py
+++ 5Midlecturequizharry_loops.py
@@ -1,4 +1,4 @@
 list =["a","b",9,3,0,14,"i",16,78,"c","f"]
 for items in list:
-    if str(items).isnumeric() and items>6:
-        print(items)
+    if str(item).isnumeric() and item>6:
+        print(item)
