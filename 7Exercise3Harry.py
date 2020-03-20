6d87582e507b292f793131231d4a5ed8384d6869
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-21 01:02:50 +0530

    Create 7Exercise3Harry.py

---
+++ 7Exercise3Harry.py
@@ -0,0 +1,31 @@
+n=18
+g = "Wrong guess, "
+h = "you have "
+i = 5
+j = " guesses"
+k = "left"
+l = "."
+print("You have", i, j+l)
+x = int(input("Guess your first no.\n"))
+
+while(i>0):
+
+    if x < 18:
+        i = i - 1
+        y = i
+        if y == 0:
+            print("Game Over")
+        else:
+            print(g, h, i, j, k+l)
+            x = int(input("Now guess a larger no.\n"))
+    elif x > 18:
+        i = i - 1
+        y = i
+        if y == 0:
+            print("Game Over")
+        else:
+            print(g, h, i, j, k+l)
+            x = int(input("Now guess a smaller no.\n"))
+    else:
+        print("Congratulations your guess is correct!!")
+        break
