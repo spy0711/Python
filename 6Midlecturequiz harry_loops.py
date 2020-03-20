bc5b1b7daf23ce9f9fcda52d53a9fe840291b2d2
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-20 23:44:10 +0530

    Update 6Midlecturequiz harry_loops.py

--- 6Midlecturequiz harry_loops.py
+++ 6Midlecturequiz harry_loops.py
@@ -1,9 +1,9 @@
-x = int(input("Type a no."))
+x = int(input("Type a no.: "))
 if x > 100:
-    print("Congratulations your no. is greater than 100")
+    print("Congratulations your no. is greater than 100.")
 else:
     while(x < 101):
-        x = int(input("Type another no."))
+        x = int(input("Type another no.: "))
         if x > 100:
-            print("Congratulations your no. is greater than 100")
+            print("Congratulations your no. is greater than 100.")
             break
