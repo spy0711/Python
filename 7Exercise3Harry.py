17e5282ec0b8129dbcd5b75aa479008d39342ecb
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-21 01:11:43 +0530

    Update 7Exercise3Harry.py

--- 7Exercise3Harry.py
+++ 7Exercise3Harry.py
@@ -1,11 +1,11 @@
 n=18
-g = "Wrong guess, "
-h = "you have "
+g = "Wrong guess,"
+h = "you have"
 i = 5
-j = " guesses"
+j = "guesses"
 k = "left"
 l = "."
-print("You have ", i, j+l)
+print("You have", i, j+l)
 x = int(input("Guess your first no.\n"))
 
 while(i>0):
@@ -15,6 +15,9 @@ while(i>0):
         y = i
         if y == 0:
             print("Game Over")
+        elif y== 1:
+            print(g, h, i, "guess", k+l)
+            x = int(input("Now guess a smaller no.\n"))
         else:
             print(g, h, i, j, k+l)
             x = int(input("Now guess a larger no.\n"))
@@ -23,6 +26,9 @@ while(i>0):
         y = i
         if y == 0:
             print("Game Over")
+        elif y== 1:
+            print(g, h, i, "Guess", k+l)
+            x = int(input("Now guess a smaller no.\n"))
         else:
             print(g, h, i, j, k+l)
             x = int(input("Now guess a smaller no.\n"))
