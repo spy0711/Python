ea8dd7dc6b423e5dd9f7f999678f21097e2c892f
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-22 15:34:19 +0530

    Update 7Exercise3Harry.py

--- 7Exercise3Harry.py
+++ 7Exercise3Harry.py
@@ -5,11 +5,12 @@ k = "left"
 l = "."
 print("Welcome to the game 'Guess the No.', a game created by Harry Bhai.\nThe value you choose should be an integer\nYou have a total of", i, j+l)
 x = int(input("Guess your first no.:\n"))
-
+u = 1
 while(i>0):
 
     if x < 18:
         i = i - 1
+        u = 6 - i
         y = i
         if y == 0:
             print("Game Over!")
@@ -21,6 +22,7 @@ while(i>0):
             x = int(input("Now guess a larger no.!\n"))
     elif x > 18:
         i = i - 1
+        u = 6 - i
         y = i
         if y == 0:
             print("Game Over!")
@@ -31,5 +33,14 @@ while(i>0):
             print(h, i, j, k+l)
             x = int(input("Now guess a smaller no.!\n"))
     else:
-        print("Congratulations your guess is correct!!")
+        if u == 1:
+            print("Congratulations your",str(u)+"st guess is correct!!")
+        elif u == 2:
+            print("Congratulations your", str(u) + "nd guess is correct!!")
+        elif u == 3:
+            print("Congratulations your", str(u) + "rd guess is correct!!")
+        elif u == 5:
+            print("Congratulations your", str(u) + "th i.e. last guess is correct!!")
+        else:
+            print("Congratulations your",str(u)+"th guess is correct!!")
         break
