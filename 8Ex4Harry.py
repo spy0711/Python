ca84b0ede991a43a1847ecf4cb075d4de10beac6
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-23 20:52:39 +0530

    Update 8Ex4Harry.py

--- 8Ex4Harry.py
+++ 8Ex4Harry.py
@@ -1,11 +1,24 @@
 n = int(input("Enter a no.:\n"))
 b = input("Enter a boolean:\n")
-i=1
+
+
+print(b)
+ if b == "True" or "1":
+     b = True
+     print("its true")
+ elif b == "False" or "0":
+     b = False
+     print("its false")
+i = 1
 try:
-    if b == "True" or "1" :
-        while i<n:
-            print("*"*i)
+    if b == True:
+        while i < n+1:
+            print("f*"*i)
             i += 1
+    elif b == False:
+        while n > 0:
+            print("s*"*n)
+            n -= 1
 except Exception as e:
     print("Error:",e,"\nTry using True with Capital 'T' or use '1' instead")
 
