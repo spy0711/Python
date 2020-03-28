efaa1316a201797c0d82adf29a4c26a593a2ca3f
Shbhm-0711 <shubhammyplaystore@gmail.com>
2020-03-29 03:47:55 +0530

    Update 9Ex5Harry.py

--- 9Ex5Harry.py
+++ 9Ex5Harry.py
@@ -17,23 +17,23 @@ def  we(a):
         h3o = h3.read()
         h3.close()
         return h3o
-def  wer(d):
-    if d == 1:
+def  wer(a, d):
+    if a == 1:
         f1 = open("99Harry" + b + ".txt", "a")
-        f1.append(getdate, y)
+        f1.append(getdate, d)
         f1.close()
-    elif d == 2:
+    elif a == 2:
         f2 = open("99Rohan" + b + ".txt", "a")
-        f2.append(getdate, y)
+        f2.append(getdate, d)
         f2.close()
-    elif d == 3:
+    elif a == 3:
         f3 = open("99Hammad" + b + ".txt", "a")
-        f3.append(getdate, y)
+        f3.append(getdate, d)
         f3.close()
 c = int(input("1 to Read, 2 for writing\n"))
 b = input("Input Exercise or Diet\n")
 a = int(input("1 for Harry, 2 for Rohan, 3 for Hammad\n"))
-d = a
+d = input("if you're writing then write it, otherwise hit enter\n")
 if c == 1:
     print(we(a))
 elif c == 2:
