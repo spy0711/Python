614ff02df2369ca86dd72e8d598be504a3bb6a8e
Gopalbansal8106 <68762809+Gopalbansal8106@users.noreply.github.com>
2020-10-01 09:20:26 +0530

    Update 6Midlecturequiz harry_loops.py

--- 6Midlecturequiz harry_loops.py
+++ 6Midlecturequiz harry_loops.py
@@ -1,9 +1,12 @@
+#to make game more interesting use random module
+import random
+random = random.randint(0,1000)
 x = int(input("Type a no.: "))
-if x > 100:
+if x > random:
     print("Congratulations your guess is greater than 100.")
 else:
-    while(x < 101):
+    while(x < random):
         x = int(input("Type another no.: "))
-        if x > 100:
+        if x > random:
             print("Congratulations your no. is greater than 100.")
             break
